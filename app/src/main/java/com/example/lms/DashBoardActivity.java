ab029bcc01312b597684aff4b9c662efb4537829
Gaurav Dixit <dgaurav300@gmail.com>
2023-09-11 17:10:27 +0530

    LMS System Project

--- app/src/main/java/com/example/lms/DashBoardActivity.java
+++ app/src/main/java/com/example/lms/DashBoardActivity.java
@@ -1,14 +1,79 @@
 package com.example.lms;
 
+<<<<<<< HEAD
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class DashBoardActivity extends AppCompatActivity {
+=======
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class DashBoardActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Toolbar toolbar;
+    private FrameLayout fragmentContainer;
+>>>>>>> 0a92542 (LMS System Project)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+<<<<<<< HEAD
+=======
+
+        // Initialize Views
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.toolbar);
+        fragmentContainer = findViewById(R.id.fragment_container);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        // Set up the Toolbar
+        setSupportActionBar(toolbar);
+
+        // Set up the ActionBarDrawerToggle for the Drawer
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Handle Bottom Navigation View item clicks
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int menu_id=item.getItemId();
+
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Open/close the Drawer when the hamburger icon is clicked
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Helper method to replace fragments in the fragment container
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+>>>>>>> 0a92542 (LMS System Project)
     }
 }

8b59944f89fa1ac581330b57d22ff45f10f306c7
Gaurav Dixit <dgaurav300@gmail.com>
2023-09-13 18:46:27 +0530

    LMS System Project

--- app/src/main/java/com/example/lms/MainActivity.java
+++ app/src/main/java/com/example/lms/MainActivity.java
@@ -1,22 +1,25 @@
 package com.example.lms;
 
-import static com.example.lms.R.id.signup1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 TextView textView;
+Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         textView = findViewById(R.id.signup1);
-        textView = findViewById(signup1);
+        btnLogin=findViewById(R.id.btnLogin);
+
+
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {

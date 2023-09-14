651d1656798d96c80cdb18a7a54fada2f48cd5c5
Gaurav Dixit <dgaurav300@gmail.com>
2023-09-14 23:51:30 +0530

    LMS System Project

--- app/src/main/java/com/example/lms/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/lms/Fragments/HomeFragment.java
@@ -2,72 +2,114 @@ package com.example.lms.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
+import com.example.lms.Adapters.ViewPagerAdapter;
+import com.example.lms.DashBoardActivity;
 import com.example.lms.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.tabs.TabLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class HomeFragment extends Fragment {
-
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-
-
-
-    }
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    ViewPagerAdapter viewPagerAdapter;
+    BottomNavigationView bottomNavigationView;
+    FrameLayout frameLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_home, container, false);
 
+        //initialize
+        tabLayout=view.findViewById(R.id.tabLayout);
+        viewPager2=view.findViewById(R.id.viewPager);
+        viewPagerAdapter=new ViewPagerAdapter(requireActivity());
+
+        viewPager2.setAdapter(viewPagerAdapter);
+
+        bottomNavigationView=view.findViewById(R.id.bottomNav);
+        frameLayout=view.findViewById(R.id.home_fragment_frame_layout);
+
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setVisibility(View.VISIBLE);
+                frameLayout.setVisibility(View.GONE);
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                viewPager2.setVisibility(View.GONE);
+                frameLayout.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                    case 1:
+                    case 2:
+                        tabLayout.getTabAt(position).select();
+
+
+                }
+                super.onPageSelected(position);
+            }
+        });
+
+bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        viewPager2.setVisibility(View.GONE);
+        frameLayout.setVisibility(View.VISIBLE);
+       switch (item.getItemId()){
+           case R.id.learningNavItem:
+               requireFragmentManager()
+                       .beginTransaction()
+                       .replace(R.id.home_fragment_frame_layout,new LearningFragment())
+                       .commit();
+               return true;
+           case R.id.assignmentNavItem:
+               requireFragmentManager()
+                       .beginTransaction()
+                       .replace(R.id.home_fragment_frame_layout,new AssignmentsFragment())
+                       .commit();
+               return true;
+           case R.id.profileNavItem:
+               requireFragmentManager()
+                       .beginTransaction()
+                       .replace(R.id.home_fragment_frame_layout,new ProfileFragment())
+                       .commit();
+               return true;
+
+       }
+
+        return true;
+    }
+});
 
 
 
